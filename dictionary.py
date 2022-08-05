7d1fbc628bc8df883500455cfcb143d06d55cfe5
tatsumaki1014 <91859537+tatsumaki1014@users.noreply.github.com>
2022-08-05 14:26:14 +0900

    Update dictionary.py
    spec5:辞書に登録されている単語にIDを振り分け

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,12 @@
 with open('dictionary-data.txt', 'r', encoding='utf-8') as f:
     word_list = f.readlines()
 
+
+id_dict = dict()
+id = 1
 for i in range(len(word_list)):
-    word_list[i] = word_list[i].replace('\n', '')
+    id_dict[word_list[i]] =  id
+    id += 1 
 
 for word in word_list:
-    print(word)
+    print(str(id_dict[word]) + ": " + word.replace("\n", "")) #replace関数で単語末尾の改行を削除(上手/n →　上手)
