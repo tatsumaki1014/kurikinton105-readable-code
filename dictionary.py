02bbfcda9392ac80268e3fcde898525e3de31a5a
Kenta Yamada <yamadakenta0101@gmail.com>
2022-08-05 13:19:01 +0900

    spec4: 複数の単語情報に対応

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,8 @@
 with open('dictionary-data.txt', 'r', encoding='utf-8') as f:
     word_list = f.readlines()
+
+for i in range(len(word_list)):
+    word_list[i] = word_list[i].replace('\n', '')
+
 for word in word_list:
     print(word)
