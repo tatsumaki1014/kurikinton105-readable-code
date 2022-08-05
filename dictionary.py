9ee9218442d6e3d7c6bee6882ee0e646a1edf916
tatsumaki1014 <91859537+tatsumaki1014@users.noreply.github.com>
2022-08-05 14:59:08 +0900

    Update dictionary.py
    spec6:ふりがな付きの辞書リストの作成(WIP)

--- dictionary.py
+++ dictionary.py
@@ -1,8 +1,13 @@
-input_id = int(input('idを入力してください'))
-
 with open('dictionary-data.txt', 'r', encoding='utf-8') as f:
-    word_list = f.readlines()
+    read_list = f.readlines()
 
+word_list = []
+kana_dict = []
+
+for i in read_list:
+    word_list_split = i.split(" ")
+    print(word_list_split)
+    
 
 id_dict = dict()
 id = 1
@@ -10,4 +15,4 @@ for i in range(len(word_list)):
     id_dict[id] = word_list[i]  
     id += 1 
 
-print(str(input_id) + ": " + str(id_dict[input_id]))
+#(WIP)
