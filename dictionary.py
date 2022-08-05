dc245cee5b75a727bc096fd33d09a1cd9e5c0612
tatsumaki1014 <91859537+tatsumaki1014@users.noreply.github.com>
2022-08-05 14:35:00 +0900

    Update dictionary.py
    spec6: 入力のidに対して対応した単語を出力

--- dictionary.py
+++ dictionary.py
@@ -1,3 +1,5 @@
+input_id = int(input('idを入力してください'))
+
 with open('dictionary-data.txt', 'r', encoding='utf-8') as f:
     word_list = f.readlines()
 
@@ -5,8 +7,7 @@ with open('dictionary-data.txt', 'r', encoding='utf-8') as f:
 id_dict = dict()
 id = 1
 for i in range(len(word_list)):
-    id_dict[word_list[i]] =  id
+    id_dict[id] = word_list[i]  
     id += 1 
 
-for word in word_list:
-    print(str(id_dict[word]) + ": " + word.replace("\n", "")) #replace関数で単語末尾の改行を削除(上手/n →　上手)
+print(str(input_id) + ": " + str(id_dict[input_id]))
